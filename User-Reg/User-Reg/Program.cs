77faba8c26ceb05cc92b90d3b27ba853b2eb2201
Arjunsing1997 <acchur1997@gmail.com>
2021-04-28 17:34:34 +0530

    UC3- Email validating..

--- User-Reg/User-Reg/Program.cs
+++ User-Reg/User-Reg/Program.cs
@@ -13,34 +13,69 @@ namespace User_Reg
         {
             Console.WriteLine("Welcome to USER-REGISTARTION");
 
-            string pattern = "^[A-Z]{1,}[A-Za-z]{2,}";
-            string firstName, lastName; 
+            string pattern = "^[A-Z]{1,}[A-Za-z]{2,}"; //for first and last name 
+            string Epattern = "^[abc]*[@a-zA-Z].[com]*"; //for Email validation Pattern
+            
+            //initializing variables
+            string firstName, lastName,eMail;     
 
             Console.WriteLine("Welcome to USER REgistration");
-
+            //for First name
             Console.WriteLine("Enter your Name: ");
             firstName = Console.ReadLine();
             ValidName(firstName, pattern);
 
+            //For last name 
             Console.WriteLine("Enter your Name: ");
             lastName = Console.ReadLine();
             ValidLastName(lastName, pattern);
 
+            //for Email 
+            Console.WriteLine("Enter your Name: ");
+            eMail = Console.ReadLine();
+            ValidEmail(eMail, Epattern);
 
             Console.ReadLine();
         }
-        public static void ValidName(string name, string pattern)
+        /// <summary>
+        /// Valids the name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="pattern">The pattern.</param>
+       public static void ValidName(string name, string pattern)
         {
             Console.WriteLine("Validating....");
             IterateLoop(name, pattern);
         }
 
+        /// <summary>
+        /// Valids the last name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="pattern">The pattern.</param>
+        /// <returns></returns>
         public static void ValidLastName(string name, string pattern)
         {
             Console.WriteLine("Validating....");
             IterateLoop(name, pattern);
         }
 
+        /// <summary>
+        /// Valids the email.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="pattern">The pattern.</param>
+        public static void ValidEmail(string name, string pattern)
+        {
+            Console.WriteLine("Validating....");
+            IterateLoop(name, pattern);
+        }
+
+        /// <summary>
+        /// Iterates the loop.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="pattern">The pattern.</param>
         public static void IterateLoop(string name, string pattern)
         {
             Regex regex = new Regex(pattern);
