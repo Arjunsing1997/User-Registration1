9fb629c2352f07b90f0388d7e93181c0a9e571f4
Arjunsing1997 <acchur1997@gmail.com>
2021-04-28 17:15:54 +0530

    UC1 first valid name

--- User-Reg/User-Reg/Program.cs
+++ User-Reg/User-Reg/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace User_Reg
 {
@@ -11,6 +12,36 @@ namespace User_Reg
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to USER-REGISTARTION");
+
+            string pattern = "^[A-Z]{1,}[A-Za-z]{2,}";
+            string firstName; 
+
+            Console.WriteLine("Welcome to USER REgistration");
+
+            Console.WriteLine("Enter your Name: ");
+            firstName = Console.ReadLine();
+            ValidName(firstName, pattern);
+
+            Console.ReadLine();
+        }
+        public static void ValidName(string name, string pattern)
+        {
+            Console.WriteLine("Validating....");
+            IterateLoop(name, pattern);
+        }
+        public static void IterateLoop(string name, string pattern)
+        {
+            Regex regex = new Regex(pattern);
+                bool result = regex.IsMatch(name);
+                if (result)
+                {
+                    Console.WriteLine("Valid Name---->" + name);
+                }
+                else
+                {
+                    Console.WriteLine("InValid Name---->" + name);
+                }
+            
         }
     }
 }
